3790209eb54a912b1c5923986c29b447ae594677
egublog <egukei0729@gmail.com>
2023-03-03 00:14:28 +0900

    modify: ApolloProviderに修正

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ const client = new ApolloClient({
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloClient client={client}>
+  <ApolloProvider client={client}>
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </ApolloClient>
+  </ApolloProvider>
 )
