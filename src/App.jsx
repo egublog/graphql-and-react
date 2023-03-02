94276b178938b7a6a23d09dd5dc57bba717d3460
egublog <egukei0729@gmail.com>
2023-03-03 00:07:29 +0900

    add: クエリの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
 import './App.css'
+import { gql } from '@apollo/client'
+
+const DOGS = gql`
+  query MyQuery {
+    dogs {
+      id
+      name
+      description
+      thumbnail {
+        url
+      }
+    }
+  }
+`
 
 function App() {
   return (
