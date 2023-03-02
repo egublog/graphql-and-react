d1a15f1c8e3c9cef96e13b948bae3c9ff5403a8a
egublog <egukei0729@gmail.com>
2023-03-03 00:05:23 +0900

    add: clientを用意

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "https://api-ap-northeast-1.hygraph.com/v2/clepsz7ri0a9301tcaprdalx8/master",
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloClient client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ApolloClient>
 )
